0752c2789b4eb6d511ddf8893a08ffdc73d3dcd7
mac <dylanmurphyDBA@gmail.com>
2020-11-17 22:05:36 -0800

    add() is somewhat working

--- bst.js
+++ bst.js
@@ -1,26 +1,76 @@
-function BTNode(value) {
-    this.val = value;
-    this.left = null;
-    this.right = null;
-}
-function BST() {
-    this.root = null;
+class BTNode {
+    constructor(value){
+        this.val = value;
+        this.count = 1;
+        this.left = null;
+        this.right = null;
+    }
     
-    function add(val){
+    increaseCount(){
+        this.count++;
+        return this;
+    }
+}
+class BST {
+    constructor(){
+        this.root = null;
+    }
+    
+    
+    add(val){
+        var newNode = new BTNode(val)
+        if(root == null){
+            this.root  = newNode;
+            return this;
+        }
+        var runner = this.root;
+        while (runner != null){
+            if (runner.val > val){
+                if(runner.left ==null){
+                    runner.left = newNode;
+                    return this;
+                }
+                else{
+                    runner = runner.left;
+                }
+            }
+            else if(runner.val == val){
+                runner.count++;
+                return this;
+            }
+            else{
+                if(runner.right == null){
+                    runner.right = newNode;
+                    return this;
+                }
+                else{
+                    runner = runner.right;
+                }
+                
+            }
+        }        
 
     }
-    function contains(val){
+    // function contains(val){
         
-    }
-    function min(){
+    // }
+    // function min(){
         
-    }
-    function max(){
+    // }
+    // function max(){
         
-    }
-    function size(){
+    // }
+    // function size(){
         
-    }
-    function isEmpty()
+    // }
+    // function isEmpty()
 
 }
+
+
+var bst1 = new BST();
+bst1.add(123);
+bst1.add(1234);
+bst1.add(23);
+bst1.add(1);
+console.log(bst1);
