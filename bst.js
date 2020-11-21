a5226109ef48257d7e909cae1979c52526d37101
mac <dylanmurphyDBA@gmail.com>
2020-11-20 19:20:46 -0800

    min and max working

--- bst.js
+++ bst.js
@@ -73,12 +73,40 @@ class BST {
         }
         return false;
     }
-    // function min(){
-        
-    // }
-    // function max(){
-        
-    // }
+    min(){
+        if(this.root == null){
+            return null;
+        }
+        else{
+            var runner = this.root;
+            while(runner != null){
+                if(runner.left == null){
+                    return runner.val;
+                }
+                else{
+                    runner = runner.left;
+                }
+                
+            }
+        }
+    }
+    max(){
+        if(this.root == null){
+            return null;
+        }
+        else{
+            var runner = this.root;
+            while(runner != null){
+                if(runner.right == null){
+                    return runner.val;
+                }
+                else{
+                    runner = runner.right;
+                }
+                
+            }
+        }
+    }
     // function size(){
         
     // }
@@ -88,14 +116,21 @@ class BST {
 
 
 var bst1 = new BST();
+bst1.add(1123999994);
+
 bst1.add(123);
 bst1.add(1234);
 bst1.add(123);
 bst1.add(12234);
 bst1.add(12333);
 bst1.add(11234);
+bst1.add(99994);
+bst1.add(1);
 
 
 console.log(bst1);
 console.log(bst1.contains(123));
 console.log(bst1.contains(112341214));
+console.log(bst1.min())
+console.log(bst1.max())
+
